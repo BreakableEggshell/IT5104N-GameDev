51af6cd6aef62671f3eaf1ab79329680044580fd
BreakableEggshell <triciagubatonmc64@gmail.com>
2026-02-25 22:20:07 +0800

    Added call_deferred to delay a method call

--- week1Act2LevelDesign/Script/hurt_box.gd
+++ week1Act2LevelDesign/Script/hurt_box.gd
@@ -2,4 +2,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().reload_current_scene()
+		call_deferred("_scene_reload")
+
+func _scene_reload():
+	get_tree().reload_current_scene()
